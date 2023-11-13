0690885332f403d2f878548a63d6601f3db4c773
Thomas Wilson <t.wilson@dundeeandangus.ac.uk>
2023-11-13 00:23:18 +0000

    first commit

--- Program.cs
+++ Program.cs
@@ -1,234 +1,137 @@
-﻿namespace tbag
+﻿using System.ComponentModel.Design;
+using System.Reflection;
+
+namespace TBAG
 {
 	internal class Program
 	{
-		static Dictionary<string, Dictionary<string, object>> ROOMS = new Dictionary<string, Dictionary<string, object>>
-	{
-		{ "start", new Dictionary<string, object>
-			{
-				{"title", "The Start!" },
-				{"desc", "You are at the start of a maze" },
-				{"east", "room1" }
-			}
-		},
-		{ "room1", new Dictionary<string, object>
-			{
-				{"title", "You are in room 1" },
-				{"desc", "The first room is here - try jumping" },
-				{"items", new List<string> { "umbrella" } },
-				{"west", "start" },
-				{"south", "room2" }
-			}
-		},
-		{ "room2", new Dictionary<string, object>
-			{
-				{"title", "You are in room 1" },
-				{"desc", "You are in another room" },
-				{"north", "room1" },
-				{"east", "end" }
-			}
-		},
-		{ "end", new Dictionary<string, object>
-			{
-				{"title", "You have escaped" },
-				{"desc", "The maze has ended!" },
-				{"west", "room2" }
-			}
-		},
-		{ "secret", new Dictionary<string, object>
-			{
-				{"title", "You are in the Secret Room" },
-				{"desc", "You found a secret room. Continue south to finish your quest" },
-				{"south", "end" }
-			}
-		}
-	};
 
-		static Dictionary<string, Dictionary<string, string>> ITEMS = new Dictionary<string, Dictionary<string, string>>
-	{
-		{ "umbrella", new Dictionary<string, string>
-			{
-				{"name", "An umbrella" },
-				{"desc", "A cute umbrella with the words M.POPPINS engraved on it." }
-			}
-		}
-	};
+		/*   Create a Maze
+		 *   
+		 *   Movement Commands - North, East, West, South
+		 *    
+		 *   S - R1 - R2   R5 - End
+		 *            |    | 
+		 *            R3 - R4 - D
+		 *            
+		 *  Each room is a dictionary of Title, Description and directions.
+		 *  The rooms are stored in a larger dictionary called ROOMS
+		 */
 
-		static string[] COMMANDS = { "north", "south", "east", "west", "jump", "look", "help", "get", "inventory" };
-		static string[] MOVEMENT = { "north", "south", "east", "west" };
-
-		// display_ROOMS
-		// function to display the current ROOMS
-		static void DisplayRooms(string loc)
+		static Dictionary<string, Dictionary<string, string>> ROOMS = new Dictionary<string, Dictionary<string, string>>
 		{
-			Console.WriteLine(ROOMS[loc]["title"]);
-			Console.WriteLine(ROOMS[loc]["desc"]);
-			if (ROOMS[loc].ContainsKey("items"))
-			{
-				Console.WriteLine("You See...");
-				foreach (var thing in (List<string>)ROOMS[loc]["items"])
+			//Start
+			{"start", new Dictionary<string, string>
 				{
-					Console.WriteLine($"\t{ITEMS[thing]["name"]}");
+					{"title", "The Start of the Maze" },
+					{"desc", "You are at the start of a large maze" },
+					{"east", "maze" },
+				}
+			},
+
+			//ROOM1
+			{"maze", new Dictionary<string, string>
+				{
+					{"title", "Lost in the Maze" },
+					{"desc", "Ah crap! You're lost in the maze!" },
+					{"west", "start" },
 				}
 			}
-			Console.WriteLine("Obvious exits are:");
-			if (ROOMS[loc].ContainsKey("north"))
-				Console.WriteLine($"\tnorth: {ROOMS[loc]["north"]}");
-			if (ROOMS[loc].ContainsKey("east"))
-				Console.WriteLine($"\teast: {ROOMS[loc]["east"]}");
-			if (ROOMS[loc].ContainsKey("south"))
-				Console.WriteLine($"\tsouth: {ROOMS[loc]["south"]}");
-			if (ROOMS[loc].ContainsKey("west"))
-				Console.WriteLine($"\twest: {ROOMS[loc]["west"]}");
-		}
+		};
 
-		// JUMP COMMAND
-		// jump in the location
-		// check the inventory if required.
-		static string JumpCommand(string loc, List<string> inv)
+		static List<string> COMMANDS = new List<string>
 		{
-			if (loc == "room1")
+			"north", "south", "east", "west", "look",
+		};
+
+
+		static void DisplayRoom(string location)
+		{
+			Console.WriteLine(ROOMS[location]["title"]);
+			Console.WriteLine(ROOMS[location]["desc"]);
+            Console.WriteLine("Obvious Exists are:");
+            if (ROOMS[location].ContainsKey("north") )
 			{
-				if (inv.Contains("umbrella"))
-				{
-					Console.WriteLine("You jump up high\nThe umbrella suddenly opens and you float down safely");
-					Console.WriteLine("You land carefully and notice you are somewhere completely different!");
-					return "secret";
-				}
-				else
-				{
-					Console.WriteLine("You jump up high\nYou begin to fall, fall, fall...");
-					Console.WriteLine(">SPLAT< - not a textbook landing");
-					KillPlayer("Landed on your head");
-				}
+                Console.WriteLine($"\tnorth: {ROOMS[location]["north"]}");
+            }
+			if (ROOMS[location].ContainsKey("east"))
+			{
+				Console.WriteLine($"\teast:  {ROOMS[location]["east"]}");
 			}
-			Console.WriteLine("You jump about menacingly");
-			return loc;
+			if (ROOMS[location].ContainsKey("south"))
+			{
+				Console.WriteLine($"\tsouth: {ROOMS[location]["south"]}");
+			}
+			if (ROOMS[location].ContainsKey("west"))
+			{
+				Console.WriteLine($"\twest:  {ROOMS[location]["west"]}");
+			}
 		}
 
-		// Kill Player
-		// Death is inevitable
-		static void KillPlayer(string reason)
-		{
-			Console.WriteLine(new string('-', 80));
-			Console.WriteLine($"\tYou have died - {reason}");
-			Console.WriteLine(new string('-', 80));
-			Environment.Exit(0);
-		}
-
-		// GET INPUT
-		// function to get the input and split the command line
-		static string[] GetInput()
+		static string[] GetPlayerInput()
 		{
 			while (true)
 			{
                 Console.Write(">> ");
-                string[] usercommand = Console.ReadLine().ToLower().Split(" ", 2);
-				if (Array.IndexOf(COMMANDS, usercommand[0]) > -1)
-				{
-					return usercommand;
+                string input = Console.ReadLine().ToLower();
+				string[] inputsplit = input.Split(" ");
+
+				if (COMMANDS.Contains(inputsplit[0])){
+					return inputsplit;
 				}
 				else
 				{
-					Console.WriteLine("I don't know that command");
-				}
+                    Console.WriteLine("I don't know what that means!");
+                }
 			}
 		}
 
-		// GET ITEM
-		// pick up an item and put it in inventory
-		static string GetCommand(string loc, string thing)
+		static string TravelTo(string currentlocation, string direction)
 		{
-			if (ITEMS.ContainsKey(thing))
+			if (ROOMS[currentlocation].ContainsKey(direction))
 			{
-				if (((List<string>)ROOMS[loc]["items"]).Contains(thing))
-				{
-					Console.WriteLine($"You pick up the {thing}");
-					((List<string>)ROOMS[loc]["items"]).Remove(thing);
-					return thing;
-				}
-				else
-				{
-					Console.WriteLine($"The {thing} isn't here!");
-					return "";
-				}
+				Console.WriteLine($"You travel {direction}");
+				return ROOMS[currentlocation][direction];
 			}
 			else
 			{
-				Console.WriteLine($"The {thing} doesn't exist!");
-				return "";
+				Console.WriteLine("You can't move in that direction");
+				return currentlocation;
 			}
 		}
 
-		// TRAVEL from loc to direction
-		// function to check if travel is possible and return new location
-		// return "" if not successful.
-		static string Travel(string loc, string direction)
+
+		static void StartGame()
 		{
-			if (ROOMS[loc].ContainsKey(direction))
+			string currentLocation = "start";
+			DisplayRoom(currentLocation);
+
+			while (true)
 			{
-				Console.WriteLine($"You move {direction}");
-				return (string)ROOMS[loc][direction];
-			}
-			else
-			{
-				Console.WriteLine($"You cannot go {direction}");
-				return "";
+				string[] commandList = GetPlayerInput();
+				switch (commandList[0])
+				{
+					case "north":
+					case "east":
+					case "south":
+					case "west":
+						currentLocation = TravelTo(currentLocation, commandList[0]);
+						DisplayRoom(currentLocation);
+						break;
+					case "look":
+						DisplayRoom(currentLocation);
+						break;
+					default: 
+						Console.WriteLine("I don't know what that is!");
+						break;
+				}
 			}
 		}
 
-		// MAIN FUNCTION
-		// set up and game loop
+
 		static void Main(string[] args)
 		{
-			int strength = 200;
-			string loc = "start";
-			List<string> inventory = new List<string>();
-
-			DisplayRooms(loc);
-			while (strength > 0)
-			{
-				string[] command = GetInput();
-
-				if (Array.IndexOf(MOVEMENT, command[0]) > -1)
-				{
-					string newloc = Travel(loc, command[0]);
-					if (newloc != "")
-					{
-						loc = newloc;
-						DisplayRooms(loc);
-					}
-				}
-				if (command[0] == "get")
-				{
-					string thing = GetCommand(loc, command[1]);
-					if (thing != "")
-					{
-						inventory.Add(thing);
-					}
-				}
-				if (command[0] == "inventory")
-				{
-					Console.WriteLine(string.Join(", ", inventory));
-				}
-				if (command[0] == "help")
-				{
-					Console.WriteLine(string.Join(", ", COMMANDS));
-				}
-				if (command[0] == "jump")
-				{
-					string newloc = JumpCommand(loc, inventory);
-					if (newloc != loc)
-					{
-						loc = newloc;
-						DisplayRooms(loc);
-					}
-				}
-				if (command[0] == "look")
-				{
-					DisplayRooms(loc);
-				}
-			}
+			StartGame();
 		}
 	}
 }
